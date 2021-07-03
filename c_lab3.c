c7a27027d9916024f6c0849df065f18b1fc31e8e
Novneet kour <75745981+111developer@users.noreply.github.com>
2021-07-03 20:20:11 +0530

    program to check if a given expression is
    correctly parenthesized

--- c_lab3.c
+++ c_lab3.c
@@ -3,26 +3,31 @@
 #include <string.h>
 struct Stack
 {
-    int *data;
-    int size; //intially size is 0.
+    char *data;
+    int size; //present size of stack
+    int top;
 };
-int push(struct Stack *stack, int value)
+int isFull(struct Stack *stack)
 {
-    if (stack->size == 0)
+    if (stack->top == stack->size - 1)
     {
-        stack->data = (int *)malloc(sizeof(int)); //allocate memory when size is zero
+        return 1;
     }
     else
+        return 0;
+}
+void push(struct Stack *stack, char value)
+{
+    if (isFull(stack))
     {
-        stack->data = (int *)realloc(stack->data, (stack->size) + 1 * sizeof(int));
+        printf("Stack Overflow!!\n");
     }
-    (stack->size)++;
-    *(stack->data + (stack->size) - 1) = value; //inserting value in  stack;
-    return stack->size;                         //returning the size of stack
+    stack->top++;
+    stack->data[++stack->top] = value;
 }
 int isEmpty(struct Stack *stack) // function to check if the stack is empty or not
 {
-    if (stack->size == 0)
+    if (stack->top == -1)
     {
         return 1;
     }
@@ -32,26 +37,33 @@ int isEmpty(struct Stack *stack) // function to check if the stack is empty or n
     }
 }
 
-void pop(struct Stack *stack)
+char pop(struct Stack *stack)
 {
+    char valueRemoved;
     if (isEmpty(stack))
     {
         printf("stack is empty");
+        return '\0';
     }
     else
     {
-        (stack->size)--;
+        valueRemoved = stack->data[stack->top];
+        (stack->top)--;
+      return valueRemoved;
     }
 }
 int main()
 {
     struct Stack stackA;
     stackA.size = 0; //intial size is 0
-    char exp[50];    //store the value of expression
-    int maxSize = strlen(exp);
+    stackA.top = -1;
+    char exp[50]; //store the value of expression
     int i;
     printf("enter the expression\n");
     scanf("%s", exp);
+    int maxSize = strlen(exp);
+    stackA.size = maxSize;
+    stackA.data = (char *)malloc(stackA.size * sizeof(char));
     for (i = 0; i < maxSize; i++)
     {
         if (exp[i] == '[' || exp[i] == '{' || exp[i] == '(')
@@ -61,17 +73,18 @@ int main()
         }
         if (exp[i] == ']' || exp[i] == '}' || exp[i] == ')')
         {
-            if (stackA.data[stackA.size - 1] == '{' && exp[i] == '}' || stackA.data[stackA.size - 1] == '[' && exp[i] == ']' || stackA.data[stackA.size - 1] == '(' && exp[i] == ')')
+            if ((stackA.data[stackA.top] == '{' && exp[i] == '}') || (stackA.data[stackA.top] == '[' && exp[i] == ']') || (stackA.data[stackA.top] == '(' && exp[i] == ')'))
             {
                 pop(&stackA);
             }
-        }
-        else
-        {
-            push(&stackA, exp[i]);
+            else
+            {
+                printf("UNBALANCED!!! \n");
+                return 0;
+            }
         }
     }
-    if (stackA.size == 0)
+    if (stackA.top == -1)
     {
         printf("BALANCED\n");
     }
